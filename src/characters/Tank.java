05a0435ba22e046aa0af4609a64c1d256697578d
Gabriel Schuch <gksgabriel2004@gmail.com>
2025-12-23 13:17:29 -0300

    Adciona a classe abstrata do Tanque

---
+++ src/characters/Tank.java
@@ -0,0 +1,40 @@
+package characters;
+
+public abstract class Tank {
+    private int life;
+    private int speed;
+    private int gunLevel;
+
+    // Construtor
+    public Tank(int life, int speed, int gunLevel) {
+        this.life = life;
+        this.speed = speed;
+        this.gunLevel = gunLevel;
+    }
+
+    // Getters e Setters
+
+    public int getGunLevel() {
+        return gunLevel;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setGunLevel(int gunLevel) {
+        this.gunLevel = gunLevel;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
