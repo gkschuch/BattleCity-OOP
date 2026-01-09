b9d1ca556ec5df812c71620eb56d4a7d1320bff5
Gabriel Schuch <gksgabriel2004@gmail.com>
2026-01-09 20:13:03 -0300

    Adicao da classe RankingEntry para ter um modelo de dados para o ranking

---
+++ src/ranking/RankingEntry.java
@@ -0,0 +1,19 @@
+package ranking;
+
+public class RankingEntry {
+    private String name;
+    private int score;
+
+    public RankingEntry(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
