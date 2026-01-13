a66d83ca2f65f2e142f86cc59c82be9ea12dc12c
Gabriel Schuch <gksgabriel2004@gmail.com>
2026-01-12 22:53:39 -0300

    Adicao de um enum para os tipos de power-ups

---
+++ src/characters/powerups/PowerUpType.java
@@ -0,0 +1,10 @@
+package characters.powerups;
+
+public enum PowerUpType {
+    STAR, // Shot upgrade
+    HELMET, // Invulnerability
+    SHOVEL, // Base protection
+    CLOCK, // Freeze enemies
+    BOMB, // Explode all enemies
+    LIFE // Extra life
+}
