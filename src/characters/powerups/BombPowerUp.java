7451a329696ec71b3792fbaddf69c998ddb70295
Gabriel Schuch <gksgabriel2004@gmail.com>
2026-01-13 20:48:28 -0300

    Adicao do power-up Bomb, que explode todos os inimigos

---
+++ src/characters/powerups/BombPowerUp.java
@@ -0,0 +1,26 @@
+package characters.powerups;
+
+import java.util.List;
+
+import characters.TankPlayer;
+import characters.enemy.EnemyTank;
+
+public class BombPowerUp extends PowerUp {
+    private List<EnemyTank> enemies;
+
+    public BombPowerUp(int x, int y, List<EnemyTank> enemies) {
+        super(x, y, PowerUpType.BOMB);
+        this.enemies = enemies;
+    }
+
+    @Override
+    public void applyEffect(TankPlayer player) {
+        this.deactivate();
+
+        for (EnemyTank enemyTank : enemies) {
+            enemyTank.takeDamage(99999);
+        }
+        System.out.println("BOMB: All enemies destroyed");
+    }
+
+}
--- src/characters/powerups/FreezeEnemiesTask.java
+++ src/characters/powerups/FreezeEnemiesTask.java
@@ -27,7 +27,6 @@ public class FreezeEnemiesTask implements Runnable {
                 enemyTank.setFrozen(false);
             }
         }
-
     }
 
 }
